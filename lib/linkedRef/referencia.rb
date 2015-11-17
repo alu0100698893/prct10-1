75e93f459a579dfc96311c5a4deb3997e82de324
Daniel Ramos <danielramosacosta@hotmail.com>
2015-11-17 17:53:12 +0000

    Creado el constructor de referencia

--- lib/linkedRef/referencia.rb
+++ lib/linkedRef/referencia.rb
@@ -14,5 +14,16 @@
 
 module LinkedRef
 	class Referencia
+		def initialize(autores, titulo, fecha)
+			raise ArgumentError, "El autor no es un array" unless autores.is_a?(Array)
+			autores.each do |a|
+				raise ArgumentError, "Uno de los autores no es un string" unless a.is_a?(String)
+			end
+			raise ArgumentError, "El titulo no es un string" unless titulo.is_a?(String)
+			raise ArgumentError, "La fecha no es de tipo Date" unless fecha.is_a?(Date)
+			@autores = autores
+			@titulo = titulo
+			@fecha = fecha
+		end
 	end
 end
--- spec/referencia_spec.rb
+++ spec/referencia_spec.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 
 describe LinkedRef::Referencia do
 	before :each do
-		@dummy=1
+		@fecha = Date.new(2001, 2, 3)
 	end
 	describe "Existe la clase referencia" do
 		it "Debe exisistir la clase referencia" do
-			expect(LinkedRef::Referencia.new).not_to be_nil
+			expect(LinkedRef::Referencia.new(["Jose Sánchez"], "Mi Título", @fecha)).not_to be_nil
 		end
 	end
 end
