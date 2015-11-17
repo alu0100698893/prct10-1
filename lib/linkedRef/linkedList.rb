a34ce0fc36275cddc87c5046f2a931a3217811ce
Daniel Ramos <danielramosacosta@hotmail.com>
2015-11-17 15:48:40 +0000

    Refactorizado to_s y superadas las isnercioens

--- lib/linkedRef/linkedList.rb
+++ lib/linkedRef/linkedList.rb
@@ -7,7 +7,33 @@ module LinkedRef
 			@tail = nil
 		end
 		def to_s
-			return "[]"
+			iterator = @head
+			string = "["
+				while !iterator.nil?
+					string << iterator[:value].to_s
+					string << ", " unless iterator[:next] == nil
+					iterator = iterator[:next]
+				end
+			string << "]"
+			return string
+		end
+		def insert_beg(val)
+			if @head.nil?
+				@head = Struct::Node.new(val, nil, nil)
+				@tail = @head
+			else
+				@head[:last] = Struct::Node.new(val, nil, @head)
+				@head = @head[:last]
+			end
+		end
+		def insert_end(val)
+			if @head.nil?
+				@head = Struct::Node.new(val, nil, nil)
+				@tail = @head
+			else
+				@tail[:next] = Struct::Node.new(val, nil, @tail)
+				@tail = @tail[:next]
+			end
 		end
 	end
 end
--- spec/linkedList_spec.rb
+++ spec/linkedList_spec.rb
@@ -13,4 +13,14 @@ describe LinkedRef::LinkedList do
 	it "Debe existir un método para imprimir la lista" do
 		expect(@lista.to_s).to eq("[]")
 	end
+	describe "Métodos de inserción" do
+		it "Por el principio" do
+			@lista.insert_beg(4)
+			expect(@lista.to_s).to eq("[4]")
+		end
+		it "Por el final" do
+			@lista.insert_end(3)
+			expect(@lista.to_s).to eq("[3]")
+		end
+	end
 end
