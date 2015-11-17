28ed2133f05285327edb8461b2f92dfce61a34d3
Daniel Ramos <danielramosacosta@hotmail.com>
2015-11-17 19:22:21 +0000

    Corregida una falta

--- spec/articuloPeriodico_spec.rb
+++ spec/articuloPeriodico_spec.rb
@@ -28,7 +28,7 @@ describe LinkedRef::ArticuloPeriodico do
 	it 'Debe existir a clase' do
 		expect(LinkedRef::ArticuloPeriodico.new(["Nombre Apellido"], "Mi Título", @fecha, "Mi periodico")).not_to be_nil
 	end
-	escribe "Debe almacenarse correctamente la referencia" do
+	describe "Debe almacenarse correctamente la referencia" do
 		it "Referencia Berngueras" do
 			expect(@artPer1.to_s).to eq("Berngueras, J. (12 de noviembre de 2015).\nYoutube Music, la app de vídeos musicales de Google, ya se puede descargar.\nEl Periódico.")
 		end
