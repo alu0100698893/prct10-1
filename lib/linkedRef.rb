df7837516545968a1677af22b407a92121d75a53
Daniel Ramos <danielramosacosta@hotmail.com>
2015-11-17 11:20:12 +0000

    Arregalado el require del spec_helper

--- lib/linkedRef.rb
+++ lib/linkedRef.rb
@@ -1,4 +1,6 @@
 require "linkedRef/version"
+require 'linkedRef/linkedList'
+require 'linkedRef/referencia'
 
 module LinkedRef
   # Your code goes here...
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require "linkedRef/linkedList"
-require "linkedRef/referencia"
+require "linkedRef"
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
