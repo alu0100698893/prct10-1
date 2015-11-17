e562f5c39bee2fa2dc3b0f752413d388cc2171f8
Daniel Ramos <danielramosacosta@hotmail.com>
2015-11-17 18:23:21 +0000

    Realizado el constructor de ArticuloRevista

--- lib/linkedRef/articuloRevista.rb
+++ lib/linkedRef/articuloRevista.rb
@@ -19,8 +19,11 @@ require 'linkedRef/referencia'
 
 module LinkedRef
 	class ArticuloRevista < Referencia
-		def initialize(autores, titulo, fecha)
+		def initialize(autores, titulo, fecha, revista, numero, volumen=nil)
 			super(autores, titulo, fecha)
+			@revista = revista
+			@volumen = volumen
+			@numero = numero
 		end
 	end
 end
