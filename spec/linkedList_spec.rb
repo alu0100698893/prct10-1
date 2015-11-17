fe996774434fe40695890b984b6ee97c5d3ce642
Daniel Ramos <danielramosacosta@hotmail.com>
2015-11-17 15:42:37 +0000

    Creada la prueba para imprimir

--- spec/linkedList_spec.rb
+++ spec/linkedList_spec.rb
@@ -1,13 +1,16 @@
 require "spec_helper"
 
 describe LinkedRef::LinkedList do
+	before :each do
+		@lista = LinkedRef::LinkedList.new
+	end
 	it "Debe existir el nodo" do
 		expect(Struct::Node.new(1, nil)).not_to be_nil
 	end
 	it "Debe existir la clase LinkedList" do
 		expect(LinkedRef::LinkedList.new).not_to be_nil
 	end
-	describe "Métodos de inserción" do
-
+	it "Debe existir un método para imprimir la lista" do
+		expect(@lista.to_s).to eq("[]")
 	end
 end
