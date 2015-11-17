edcb18e8fc25c57909cee22c5e1c76674777c264
Daniel Ramos <danielramosacosta@hotmail.com>
2015-11-17 19:22:03 +0000

    Añadidas las pruebas del periodico

--- spec/articuloPeriodico_spec.rb
+++ spec/articuloPeriodico_spec.rb
@@ -21,8 +21,22 @@ require "spec_helper"
 describe LinkedRef::ArticuloPeriodico do
 	before :each do
 		@fecha = Date.new(2001, 2, 3)
+		@artPer1 = LinkedRef::ArticuloPeriodico.new(["Jorge Berngueras"], "Youtube Music, la app de vídeos musicales de Google, ya se puede descargar", Date.new(2015, 11, 12), "El Periódico")
+		@artPer2 = LinkedRef::ArticuloPeriodico.new(["Juan Palazon", "Laura Pajuelo"], "El primer móvil con Android One", Date.new(2015, 11, 16), "El País")
+		@artPer3 = LinkedRef::ArticuloPeriodico.new(["Carlos Fresneda"], "El Gobierno británico lleva la guerra contra el terrorismo al ciberespacio", Date.new(2015, 11, 12), "El Mundo")
 	end
 	it 'Debe existir a clase' do
 		expect(LinkedRef::ArticuloPeriodico.new(["Nombre Apellido"], "Mi Título", @fecha, "Mi periodico")).not_to be_nil
+	end
+	escribe "Debe almacenarse correctamente la referencia" do
+		it "Referencia Berngueras" do
+			expect(@artPer1.to_s).to eq("Berngueras, J. (12 de noviembre de 2015).\nYoutube Music, la app de vídeos musicales de Google, ya se puede descargar.\nEl Periódico.")
+		end
+		it "Referencia Palazon y Pajuelo" do
+			expect(@artPer2.to_s).to eq("Palazon, J., Pajuelo, L. (16 de noviembre de 2015).\nEl primer móvil con Android One.\nEl País.")	
+		end
+		it "Referencia Fresneda" do
+			expect(@artPer3.to_s).to eq("Fresneda, C. (17 de noviembre de 2015).\nEl Gobierno británico lleva la guerra contra el terrorismo al ciberespacio.\nEl Mundo")
+		end
 	end
 end
