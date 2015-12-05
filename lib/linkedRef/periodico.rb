b5d46f111df7c8895b96806c7959ca142f0aae64
Daniel Ramos <danielramosacosta@hotmail.com>
2015-12-05 19:43:03 +0000

    Terminado el periodico

--- lib/linkedRef/periodico.rb
+++ lib/linkedRef/periodico.rb
@@ -1,20 +1,18 @@
-require 'linkedRef/articulo'
+require 'linkedRef/publicacionesPeriodicas'
 
 module LinkedRef
-	class Periodico < Articulo
-		def initialize(autores, titulo, fecha, periodico)
-			super(autores, titulo, fecha)
+	class Periodico < PublicacionesPeriodicas
+		def initialize(autores, titulo, fecha, periodico, paginas)
+			formato="Papel"
+			super(autores, titulo, fecha, formato)
 			raise ArgumentError, "El periodico no es un string" unless periodico.is_a?(String)
+			raise ArgumentError, "Las paginas no es un entero" unless  paginas.is_a?(Integer)
 			@periodico = periodico
+			@paginas = paginas
 		end
 		def to_s()
 			string = ""
-			string += autores
-			string += " (" + fecha_completa + ").\n"
-			string += titulo
-			string += ".\n"
-			string += @periodico + "."
-			return string
+			string << @autores << " (" << fecha_completa(@fecha) << "). " << @titulo << ". " << @periodico << ", pp. " << @paginas.to_s << "."
 		end
 	end
 end
--- spec/periodico_spec.rb
+++ spec/periodico_spec.rb
@@ -1,45 +1,16 @@
 require "spec_helper"
 
-# Formato de revista:
-# Autor[,...]. (fecha).
-# Titulo del articulo.
-# Nombre del periodico.
-
-# Berngueras, J. (12 de noviembre de 2015).
-# Youtube Music, la app de videos musicales de Google, ya se puede descargar.
-# El Periodico.
-
-# Palazon, J., Pajuelo, L. (16 de noviembre de 2015).
-# El primer movil con Android One.
-# El Pais.
-
-# Fresneda, C. (17 de noviembre de 2015).
-# El Gobierno britanico lleva la guerra contra el terrorismo al ciberespacio.
-# El Mundo.
-
 describe LinkedRef::Periodico do
-	before :each do
-		@fecha = Date.new(2001, 2, 3)
-		@artPer1 = LinkedRef::Periodico.new(["Jorge Berngueras"], "Youtube Music, la app de videos musicales de Google, ya se puede descargar", Date.new(2015, 11, 12), "El Periodico")
-		@artPer2 = LinkedRef::Periodico.new(["Juan Palazon", "Laura Pajuelo"], "El primer movil con Android One", Date.new(2015, 11, 16), "El Pais")
-		@artPer3 = LinkedRef::Periodico.new(["Carlos Fresneda"], "El Gobierno britanico lleva la guerra contra el terrorismo al ciberespacio", Date.new(2015, 11, 17), "El Mundo")
+	before :all do
+		@fecha = Date.new(2015, 11, 15)
+		@artPer = LinkedRef::Periodico.new(["Javier Palazon", "Laura Pajuelo"], "El primer movil con Android One", @fecha, "El Pais", 68)
 	end
-	it 'Debe existir a clase' do
-		expect(LinkedRef::Periodico.new(["Nombre Apellido"], "Mi Titulo", @fecha, "Mi periodico")).not_to be_nil
+	it 'Debe existir la clase' do
+		expect(LinkedRef::Periodico.new(["Nombre Apellido"], "Mi Titulo", @fecha, "Mi periodico", 23)).not_to be_nil
 	end
 	describe "Debe almacenarse correctamente la referencia" do
-		it "Referencia Berngueras" do
-			expect(@artPer1.to_s).to eq("Berngueras, J. (12 de noviembre de 2015).\nYoutube Music, la app de videos musicales de Google, ya se puede descargar.\nEl Periodico.")
+		it "Referencia de ejmplo del README" do
+			expect(@artPer.to_s).to eq("Palazon, J. & Pajuelo, L. (15 de noviembre de 2015). El Primer Movil con Android one. El Pais, pp. 68.")
 		end
-		it "Referencia Palazon y Pajuelo" do
-			expect(@artPer2.to_s).to eq("Palazon, J., Pajuelo, L. (16 de noviembre de 2015).\nEl primer movil con Android One.\nEl Pais.")	
-		end
-		it "Referencia Fresneda" do
-			expect(@artPer3.to_s).to eq("Fresneda, C. (17 de noviembre de 2015).\nEl Gobierno britanico lleva la guerra contra el terrorismo al ciberespacio.\nEl Mundo.")
-		end
-	end
-	it "Debe poder ser comparable" do
-		expect(@artPer1>@artPer2).to be true
-		expect(@artPer2<@artPer1).to be true
 	end
 end
